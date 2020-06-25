f9067b812182321d74a65e442b0cea26053d9306
ZaurBegiev <whereistheslim@gmail.com>
2020-06-25 23:26:54 +0300

    retries exception

--- courseraparser.py
+++ courseraparser.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-from time import sleep
 import numpy as np
-from functools import reduce
 import pandas as pd
+from functools import reduce
+from time import sleep
 
 
 class CatalogueParser:
@@ -19,11 +19,10 @@ class CatalogueParser:
             'students': '.enrollment-number',
             'level': '.difficulty',
         }
-        self.columns = list(self.selectors.keys()) + ['link']
+        self.columns = list(self.selectors.keys()) + ['path']
 
     def get_number_of_pages(self):
-        r = requests.get(self.initial_url)
-        soup = BeautifulSoup(r.content, features='html.parser')
+        soup = self._get_page_soup(1)
         return int(soup.select('.box.number')[-1].text)
 
     def parse_page(self, page):
@@ -53,15 +52,86 @@ class CatalogueParser:
         for key in self.selectors.keys():
             content = soup.select(self.selectors[key])
             page_data.append([el.text for el in content])
-        page_data.append(self._get_links(soup))
+        page_data.append(self.get_paths(soup))
         sleep(1)
         return list(zip(*page_data))
 
     @staticmethod
-    def _get_links(soup):
+    def get_paths(soup):
         content = soup.find_all('a', class_="rc-DesktopSearchCard anchor-wrapper")
         return [tag.get('href') for tag in content]
 
 
-my_parser = CatalogueParser()
-my_parser.parse(3)
+# my_parser = CatalogueParser()
+# print(my_parser.get_number_of_pages())
+
+
+class ReviewParser:
+    def __init__(self):
+        self.home = 'https://www.coursera.org'
+
+    def get_soup(self, path, add=''):
+        url = f'{self.home}{path}{add}'
+        r = requests.get(url)
+        sleep(1)
+        soup = BeautifulSoup(r.content, features='html.parser')
+        return soup
+
+    def get_skills(self, path):
+        soup = self.get_soup(path)
+        content = soup.select('._rsc0bd.m-r-1s.m-b-1s')
+        content = [tag.get('title') for tag in content]
+        return tuple(content)
+
+    def get_estimated_time(self, path):
+        soup = self.get_soup(path)
+        content = soup.select('._16ni8zai.m-b-0.m-t-1s')[0]
+        return content.text
+
+    def get_language(self, path):
+        soup = self.get_soup(path)
+        content = soup.select('._1tu07i3a')
+        content = [tag.text for tag in content]
+        content = [el for el in content if el.find('Subtitles') != -1]
+        if len(content) > 1:
+            content = content[0]
+        return content
+
+    def get_reviews(self, path):
+        content = {}
+
+        for star in range(1, 2):            
+            soup = self.get_soup(path, add=f'/reviews?sort=helpful&star={star}')
+            number_of_pages = self.get_number_of_review_pages(soup)
+
+            content[star] = []
+
+            for page in range(1, number_of_pages + 1):
+                url = f'{self.home}{path}/reviews?sort=helpful&star={star}'
+                if page > 1:
+                    url += f'&page={page}'
+                soup = self.get_soup(url)
+                thumbs_up = self.get_review_thumbs_up(soup)
+                texts = soup.select('.rc-CML.font-lg.styled')
+                texts = [tag.text for tag in texts]
+                content[star].extend(zip(texts, thumbs_up))
+
+        return content
+
+    @staticmethod
+    def get_number_of_review_pages(soup):
+        content = soup.select('._b0s5mt2')
+        content = [el.text for el in content]
+        return int(content[-1])
+
+    @staticmethod
+    def get_review_thumbs_up(soup):
+        content = soup.select('._1lutnh9y')
+        content = [tag.text for tag in content]
+        content = [el for el in content if el.find('Thumbs UpThis is helpful') != -1]
+        return content
+
+
+my_parser = ReviewParser()
+my_path = '/learn/the-science-of-well-being'
+print(my_parser.get_reviews(my_path))
